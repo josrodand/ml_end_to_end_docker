9823976b47f7b227db5ddd05a5d6e4eff89fee12
Selu <josrodand@gmail.com>
2023-07-24 11:40:50 +0200

    Creating and testing api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from Flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
 
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 # load model
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,4 +17,11 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
-    
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == '__main__':
+    app.run(debug=True)
