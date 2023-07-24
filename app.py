61293ff3bb7b2933edac01df72f51ede9c517cfc
Selu <josrodand@gmail.com>
2023-07-24 11:13:14 +0200

    added scaling pickle file

---
+++ app.py
@@ -0,0 +1,19 @@
+import pickle
+from Flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+# load model
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    
