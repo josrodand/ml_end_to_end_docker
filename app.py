04f5bdfb1cc7d958596a60c9d7ce84d82dd73a95
Selu <josrodand@gmail.com>
2023-07-24 14:47:16 +0200

    Added html template for front app

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
 scalar = pickle.load(open('scaling.pkl', 'rb'))
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
@@ -22,6 +24,15 @@ def predict_api():
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text = f"The house price prediction is {output}")
 
 if __name__ == '__main__':
     app.run(debug=True)
